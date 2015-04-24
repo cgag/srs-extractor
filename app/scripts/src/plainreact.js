7c2dd887fc6cffeff57ce080b22cccd69e170190
Curtis Gagliardi <curtis@curtis.io>
2015-04-23 21:07:43 -0700

    truncation, formatting

--- app/scripts/src/plainreact.js
+++ app/scripts/src/plainreact.js
@@ -21,17 +21,18 @@ function isBlank(s: string): boolean {
   return (!s || /^\s*$/.test(s));
 }
 
+truncate(s: string): string {
+  var maxChars = 20
+  if (s.length > maxChars) {
+    return s.substring(0, maxChars) + '...'
+  } else {
+    return s
+  }
+}
+
 var CardList = React.createClass({
   propTypes: {
     cards: PT.array.isRequired
-  },
-  truncate(s: string): string {
-    var maxChars = 20
-    if (s.length > maxChars) {
-      return s.substring(0, maxChars) + '...'
-    } else {
-      return s
-    }
   },
   render(): any {
     var cards = this.props.cards.map((card, i) => {
@@ -39,7 +40,7 @@ var CardList = React.createClass({
     })
     return (
       <div className="cardList">
-        <ul> 
+        <ul>
           {cards}
         </ul>
       </div>
@@ -60,9 +61,9 @@ var SRSBox = React.createClass({
 
     // TODO: get bindings from config page
     Mousetrap.bind('ctrl+i', this.insertSelection)
-    Mousetrap.bind('ctrl+;', () => { 
+    Mousetrap.bind('ctrl+;', () => {
       this.setState({visible: !this.state.visible})
-      this.storeState(this.state) 
+      this.storeState(this.state)
     })
 
     // saving cards doesn't set state, just modifies storage, we set
@@ -85,7 +86,7 @@ var SRSBox = React.createClass({
   saveCard(card: Card): void {
     var currCard = this.state.currentCard
     if(!isBlank(currCard.text)) {
-      this.storeState(A.update(this.state,  
+      this.storeState(A.update(this.state,
                             { savedCards:  {$unshift: [currCard]}
                             , currentCard: {$set: defaultCard}}))
     }
@@ -95,23 +96,20 @@ var SRSBox = React.createClass({
   },
   makeCloze(): void {
     var clozeSel = function(s, selection, clozeNum) {
-      var selectedText = selection.text
-      var start = selection.start
-      var end = selection.end
       if (!isBlank(selectedText)) {
-        return s.substring(0, start) 
-               + '{{c' 
+        return s.substring(0, selection.start)
+               + '{{c'
                + clozeNum
                + '::'
-               + selectedText 
-               + '}}' 
-               + s.substring(end)
+               + selection.text
+               + '}}'
+               + s.substring(selection.end)
       } else {
         return s
       }
     }
 
-    var clozeText = 
+    var clozeText =
       clozeSel( this.state.currentCard.text
               , $(this.refs.srsTextarea.getDOMNode()).getSelection()
               , this.state.currentCard.numClozes)
@@ -120,36 +118,38 @@ var SRSBox = React.createClass({
     // numClozes correct if nothing is selected.
     if (clozeText !== this.state.currentCard.text) {
       this.setState(A.update(this.state,
-        {currentCard: 
+        {currentCard:
           { text: {$set: clozeText }
           , numClozes: {$apply: R.inc }}}))
     }
   },
   downloadCards(): void {
     function download(filename, text) {
-      var pom = document.createElement('a');
-      pom.setAttribute('href'
+      var dlLink = document.createElement('a');
+      dlLink.setAttribute('href'
           , 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-      pom.setAttribute('download', filename);
-      pom.click();
+      dlLink.setAttribute('download', filename);
+      dlLink.click();
     }
-    
+
     var ankiFormattedCards = this.state.savedCards.map((card) => {
       return '\"' + card.text + '\";'
     }).join('\n')
-    
+
+    this.setState({savedCards: []})
+
     download('exportForAnki.txt', ankiFormattedCards)
   },
   handleInput(event): void {
-    this.setState(A.update(this.state, 
-                            {currentCard: 
+    this.setState(A.update(this.state,
+                            {currentCard:
                               {text: {$set: event.target.value}}}))
   },
   render(): any {
     return !this.state.visible ? null :
       <div id='srsContainer'>
-        <textarea value={this.state.currentCard.text} 
-                  onChange={this.handleInput} 
+        <textarea value={this.state.currentCard.text}
+                  onChange={this.handleInput}
                   ref='srsTextarea' />
         <div className="srsButtons">
           <button onClick={this.insertSelection}>Insert</button>
